6928c7680bc188228dd2ce4a1440659266abc1da
Wender Natanael <wendermarquesmarques@gmail.com>
2022-05-24 13:46:43 -0300

    Novo projeto: livros

--- desafio/site-quimica/js/index.js
+++ desafio/site-quimica/js/index.js
@@ -8,7 +8,6 @@ var body = window.document.querySelector('body')
 var tema = window.document.querySelector('a#tema')
 var esfera1 = window.document.getElementById('esfera1')
 var esfera2 = window.document.getElementById('esfera2')
-var Vtema = true
 
 //body.style.backgroundImage = 'linear-gradient(to top, rgb(134 10 10), red, rgb(255 235 0))'
 
@@ -75,7 +74,10 @@ fecharmenu.addEventListener('click', function() {
     }
 })
 
+let Vtema = localStorage.Vtema
+console.log(Vtema);
 tema.addEventListener('click', function() {
+    Vtema = true
     if(Vtema === false) {
         body.style.background = '#100636'
         menu.style.background = '#100636'
@@ -83,7 +85,7 @@ tema.addEventListener('click', function() {
         esfera2.style.backgroundImage = 'linear-gradient(to top, rgb(10, 12, 134), blue, rgb(0, 255, 179))'
 
         Vtema = true
-        console.log(Vtema)
+
     } else {
         body.style.background = '#830505'
         menu.style.background = '#830505'
@@ -91,6 +93,9 @@ tema.addEventListener('click', function() {
         esfera2.style.backgroundImage = 'linear-gradient(to top, rgb(134 10 10), red, rgb(255 235 0))'
 
         Vtema = false
-        console.log(Vtema)
     }
+    
+    console.log(Vtema)
+    localStorage.setItem('Vtema', Vtema) 
 })
+
