0f55f1c2374c5431f0d8696a3710c6892f5cccdd
Wender Natanael <wendermarquesmarques@gmail.com>
2022-05-27 11:59:50 -0300

    a

--- desafio/site-quimica/js/index.js
+++ desafio/site-quimica/js/index.js
@@ -5,7 +5,7 @@ var fecharmenu = window.document.querySelector('button#fecharmenu')
 var html = window.document.querySelector('html')
 
 var body = window.document.querySelector('body')
-var tema = window.document.querySelector('a#tema')
+var btntema = window.document.querySelector('a#tema')
 var esfera1 = window.document.getElementById('esfera1')
 var esfera2 = window.document.getElementById('esfera2')
 
@@ -74,17 +74,18 @@ fecharmenu.addEventListener('click', function() {
     }
 })
 
-let Vtema = localStorage.Vtema
-console.log(Vtema);
-tema.addEventListener('click', function() {
-    Vtema = true
-    if(Vtema === false) {
+btntema.addEventListener('click', function() {
+    tema(Vtema)
+})
+
+function tema(y) {
+    if(y == false) {
         body.style.background = '#100636'
         menu.style.background = '#100636'
         esfera1.style.backgroundImage = 'linear-gradient(to top, rgb(10, 12, 134), blue, rgb(0, 255, 179))'
         esfera2.style.backgroundImage = 'linear-gradient(to top, rgb(10, 12, 134), blue, rgb(0, 255, 179))'
 
-        Vtema = true
+        y = true
 
     } else {
         body.style.background = '#830505'
@@ -92,10 +93,36 @@ tema.addEventListener('click', function() {
         esfera1.style.backgroundImage = 'linear-gradient(to top, rgb(134 10 10), red, rgb(255 235 0))'
         esfera2.style.backgroundImage = 'linear-gradient(to top, rgb(134 10 10), red, rgb(255 235 0))'
 
-        Vtema = false
+        y = false
     }
-    
+
     console.log(Vtema)
-    localStorage.setItem('Vtema', Vtema) 
-})
+    var VtemaJSON = JSON.stringify(Vtema);
+    localStorage.setItem('vtema', VtemaJSON);
+}
+
+
+function adicionaTemaSalvo() {
+    const vtema1 = localStorage.getItem('vtema');
+    const Vtema2 = JSON.parse(vtema1);
+    Vtema = Vtema2
+
+        if(Vtema === false) {
+            body.style.background = '#100636'
+            menu.style.background = '#100636'
+            esfera1.style.backgroundImage = 'linear-gradient(to top, rgb(10, 12, 134), blue, rgb(0, 255, 179))'
+            esfera2.style.backgroundImage = 'linear-gradient(to top, rgb(10, 12, 134), blue, rgb(0, 255, 179))'
+    
+            Vtema = true
+    
+        } else {
+            body.style.background = '#830505'
+            menu.style.background = '#830505'
+            esfera1.style.backgroundImage = 'linear-gradient(to top, rgb(134 10 10), red, rgb(255 235 0))'
+            esfera2.style.backgroundImage = 'linear-gradient(to top, rgb(134 10 10), red, rgb(255 235 0))'
+    
+            Vtema = false
+        }
+}
+adicionaTemaSalvo();
 
