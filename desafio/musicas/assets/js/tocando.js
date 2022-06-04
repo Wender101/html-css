561c434a0a9244c17af5980ed2464f83b18dae69
Wender Natanael <wendermarquesmarques@gmail.com>
2022-06-04 11:53:55 -0300

    aa

--- desafio/musicas/assets/js/tocando.js
+++ desafio/musicas/assets/js/tocando.js
@@ -1,9 +1,9 @@
 var time
+const  fJSON1 = localStorage.getItem('ff')
+const fJSON2 = JSON.parse(fJSON1)
 
 // Vai puxar do navegador qual foi a musica escolhida
 function qualMusica() {
-    const  fJSON1 = localStorage.getItem('ff');
-    const fJSON2 = JSON.parse(fJSON1);
     
     let musica = document.getElementById('musica')
     let musicaAtual = document.createElement('img')
@@ -29,6 +29,33 @@ function qualMusica() {
         }
     }
     criaMusica(fJSON2)
+
+    let segundos = 0
+    let input = document.getElementById('inputDentro')
+    let t = time * 6.4
+    let qSegundos = 1
+    //Inicio
+    function iniciaRelogio() {
+        setInterval(function() {
+            segundos += qSegundos
+            input.style.width = segundos + '%'
+
+            if(segundos == 100) {
+                qSegundos = 0
+                input.style.borderRadius = '10px'
+
+                setTimeout(function() {
+                    segundos = 0
+                    qSegundos = 1
+                    var f = fJSON2 + 1
+                    criaMusica(f)
+                    clicar()
+                }, 5000)
+            }
+        }, t)
+
+    }
+    iniciaRelogio()
 }
 qualMusica()
 
@@ -43,26 +70,6 @@ function pausar() {
         clicar(playNow)
     }
 }
-
-let segundos = 0
-let input = document.getElementById('inputDentro')
-let t = time * 6.4
-let qSegundos = 1
-//Inicio
-function iniciaRelogio() {
-    setInterval(function() {
-        segundos += qSegundos
-        input.style.width = segundos + '%'
-        console.log(segundos);
-        if(segundos == 100) {
-            qSegundos = 0
-            input.style.borderRadius = '10px'
-        }
-    }, t)
-
-}
-
-iniciaRelogio()
 
 function clicar(pausarOuNao = false) {
     let audio = document.querySelector('audio')
