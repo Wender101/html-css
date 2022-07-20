18fe09a032f691fba86f8205af0f7050979c0ceb
Wender Natanael <wendermarquesmarques@gmail.com>
2022-07-20 00:35:14 -0300

    a

--- desafio/Torneio/assets/js/index.js
+++ desafio/Torneio/assets/js/index.js
@@ -2,22 +2,29 @@ function tempoRestante() {
     var one_day = 1000 * 60 * 60 * 24
     var present_date = new Date();
     var diaX = new Date(present_date.getFullYear(), 7, 8)
-
+    
     if (present_date.getMonth() == 11 && present_date.getdate() > 25) diaX.setFullYear(diaX.getFullYear() + 1)
-
+    
     var Result = Math.round(diaX.getTime() - present_date.getTime()) / (one_day);
     var Final_Result = Result.toFixed(0);
-
+    
     const time = document.getElementById('time')
     const localDia = time.getElementsByTagName('span')[0]
     const localHora = time.getElementsByTagName('span')[1]
     const localMin = time.getElementsByTagName('span')[2]
     const localSeg = time.getElementsByTagName('span')[3]
-    localDia.innerText = Final_Result
-    
+
+    if(present_date.getDate() < 20) {
+        var diaInicio = 19 - present_date.getDate()
+    } else {
+        var diaInicio = present_date.getDate() - 19
+    }
+    localDia.innerText = Number(Final_Result) - diaInicio
+
     const hrTotal = 23
     const minTotal = 60
     const segTotal = 60
+
     setInterval(() => {
         let timeNow = new Date()
         let hora = hrTotal - timeNow.getHours()
