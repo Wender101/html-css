9f2d3ca40db4893acbc1e691270ba880d672c891
Wender Natanael <93059850+Wender101@users.noreply.github.com>
2022-08-01 22:47:39 -0300

    a

---
+++ desafio/sistemaOperacional/assets/js/areaDeTrabalho.js
@@ -0,0 +1,162 @@
+let menuAberto = false
+let aberto = false
+function abrirMenu() {
+    if(menuAberto == false) {
+        const menu = document.getElementById('menu')
+        menu.style.transition = '200ms bottom linear'
+        menu.style.bottom = '40px'
+
+        menuAberto = true
+
+    } else {
+        fecharMenu()
+    }
+}
+
+function fecharMenu() {
+    if(menuAberto == true) {
+        const menu = document.getElementById('menu')
+        menu.style.transition = '100ms bottom linear'
+        menu.style.bottom = '-100vh'
+
+        const opsIniciar = document.getElementById('opsIniciar')
+        opsIniciar.style.display = 'none'
+
+        menuAberto = false
+    }
+}
+
+// Ao clicar na tela
+function tela() {
+    fecharMenu()
+}
+
+// Voltar pra aréa de tarefas
+function valtarAreaDeTarefas() {
+    fecharMenu()
+    for(let c = 0; c < 100; c++) {
+        const aba = document.getElementsByClassName('aba')[c]
+        aba.style.display = 'none'
+        aberto = false
+    }
+}
+
+// Ao clicar em abrir config
+let openconfig = false
+function abrirConfig() {
+    if(openconfig == false) {
+        setTimeout(() => {
+            const pageConfig = document.getElementById('pageConfig')
+            pageConfig.style.display = 'block'
+            
+            const barraDeTarefas = document.getElementById('barraDeTarefas')
+            
+            const div = document.createElement('div')
+            div.className = 'icones'
+            div.id = 'configIcone'
+            div.style.backgroundImage = 'url(assets/img/icones/config2.png)'
+            barraDeTarefas.appendChild(div)
+            
+            div.addEventListener('click', () => {
+                if(aberto == false) {
+                    const pageConfig = document.getElementById('pageConfig')
+                    pageConfig.style.display = 'block'
+                    
+                    aberto = true
+                } else {
+                    const pageConfig = document.getElementById('pageConfig')
+                    pageConfig.style.display = 'none'
+                    
+                    aberto = false
+                }
+            })
+            openconfig = true
+        }, 500)
+    
+        fecharMenu()
+    }
+}
+
+// Ao clicar em fechar config
+function fecharConfig() {
+    const pageConfig = document.getElementById('pageConfig')
+    pageConfig.style.display = 'none'
+    const configIcone = document.getElementById('configIcone')
+    configIcone.style.display = 'none'
+    openconfig = false
+}
+
+//Vai fechar todas as abas
+function fecharAbas() {
+    for(let c = 0; c < 100; c++) {
+        const aba = document.getElementsByClassName('aba')[c]
+        aba.style.display = 'none'
+        const icones = document.getElementsByClassName('icones')[c]
+        icones.style.display = 'none'
+    }
+}
+
+function opsIniciar() {
+    const opsIniciar = document.getElementById('opsIniciar')
+    opsIniciar.style.display = 'block'
+    const lateralMenu = document.getElementById('lateralMenu')
+    lateralMenu.style.width = '200px'
+}
+
+// Funções do menu iniciar
+function suspender() {
+    setTimeout(() => {
+        fecharMenu()
+        const black = document.getElementById('black')
+        black.style.display = 'block'
+
+        setTimeout(() => {
+            document.addEventListener('click', () => {
+                const black = document.getElementById('black')
+                black.style.display = 'none'
+            })
+        
+            document.addEventListener('keydown', (e) => {
+                if(e.keyCode == 13) {
+                    const black = document.getElementById('black')
+                    black.style.display = 'none'
+                }
+            })
+
+        }, 400)
+    }, 400)
+}
+
+function desligar() {
+    setTimeout(() => {
+        fecharMenu()
+        const black = document.getElementById('black')
+        black.style.display = 'block'
+
+        setTimeout(() => {
+            document.addEventListener('click', () => {
+                location.reload()
+            })
+        
+            document.addEventListener('keydown', (e) => {
+                if(e.keyCode == 13) {
+                    location.reload()
+                }
+            })
+
+        }, 400)
+    }, 400)
+}
+
+function reiniciar() {
+    const main = document.querySelector('main')
+    main.style.display = 'none'
+
+    const body= document.querySelector('body')
+    body.style.background = 'black'
+
+    setTimeout(() => {
+        location.reload()
+
+    }, 3000)
+}
---
+++ desafio/sistemaOperacional/assets/js/telaDeBloqueio.js
@@ -0,0 +1,131 @@
+function ligando() {
+    setTimeout(() => {
+        const localLogo = document.getElementById('localLogo')
+        localLogo.style.display = 'block'
+    }, 2000)
+
+    setTimeout(() => {
+        const telaIniciando = document.getElementById('telaIniciando')
+        telaIniciando.style.display = 'none'
+        start()
+    }, 6000)
+} ligando()
+
+function start() {
+    function exibirData() {
+        // Horas
+    const tempo = new Date()
+    let hr = tempo.getHours()
+    if(hr < 10) hr = `0${hr}`
+
+    let min = tempo.getMinutes()
+    if(min < 10) min = `0${min}`
+
+    const horas = document.getElementById('horas')
+    horas.innerText = `${hr}:${min} `
+
+    const horasBarra = document.getElementById('horasBarra')
+    horasBarra.innerText = `${hr}:${min} `
+
+    // Data
+    let diaSemana = tempo.getDay()
+
+    if(diaSemana == 0) {
+        diaSemana = 'domingo'
+    } else if(diaSemana == 1) {
+        diaSemana = 'segunda-feira'
+    } else if(diaSemana == 2) {
+    diaSemana = 'terça-feira'
+    } else if(diaSemana == 3) {
+        diaSemana = 'quarta-feira'
+    } else if(diaSemana == 4) {
+        diaSemana = 'quinta-feira'
+    } else if(diaSemana == 5) {
+        diaSemana = 'sexta-feira'
+    } else if(diaSemana == 6) {
+        diaSemana = 'sabado'
+    }
+
+    let hoje = tempo.getDate()
+
+    let mes = tempo.getMonth()
+
+    if(mes == 0) {
+        mes = 'janeiro'
+    } else if(mes == 1) {
+        mes = 'fevereiro'
+    } else if(mes == 2) {
+        mes = 'março'
+    } else if(mes == 3) {
+        mes = 'abril'
+    } else if(mes == 4) {
+        mes = 'maio'
+    } else if(mes == 5) {
+        mes = 'junho'
+    } else if(mes == 6) {
+        mes = 'julho'
+    } else if(mes == 7) {
+        mes = 'agosto'
+    } else if(mes == 8) {
+        mes = 'setembro'
+    } else if(mes == 9) {
+        mes = 'outubro'
+    } else if(mes == 10) {
+        mes = 'novembro'
+    } else if(mes == 11) {
+        mes = 'dezembro'
+    }
+
+    const data = document.getElementById('data')
+    data.innerText = `${diaSemana}, ${hoje} de ${mes}`
+    }
+
+    setInterval(() => {
+        exibirData()
+    }, 100)
+
+    document.addEventListener('click', () => {
+        logar()
+    })
+
+    document.addEventListener('keydown', (e) => {
+        if(e.keyCode == 13) logar()
+    })
+
+    function logar() {
+        const title = document.querySelector('title')
+        title.innerText = 'Bem-vindo(a)'
+        const inf = document.getElementById('inf')
+        inf.style.transition = '300ms bottom linear'
+        inf.style.bottom = '120vh'
+
+        setTimeout(() => {
+            const logar = document.getElementById('logar')
+            const filter = document.getElementById('filter')
+            logar.style.transition = '300ms filter linear'
+            logar.style.filter = 'opacity(100%)'
+            filter.style.transition = '1s filter linear'
+            filter.style.filter = 'blur(6px)'
+        }, 1000)
+
+        let timeIniciar = Math.floor(Math.random() * 5000)
+
+        if(timeIniciar < 2000) timeIniciar = 2000
+
+        setTimeout(() => {
+
+            const logar = document.getElementById('logar')
+            const filter = document.getElementById('filter')
+            logar.style.transition = '300ms filter linear'
+            logar.style.filter = 'opacity(0%)'
+            filter.style.transition = '300ms filter linear'
+            filter.style.filter = 'opacity(0%)'
+
+            const telaDeBloqueio = document.getElementById('telaDeBloqueio')
+            telaDeBloqueio.style.display = 'none'
+
+            const areaDeTrabalho = document.getElementById('areaDeTrabalho')
+            areaDeTrabalho.style.display = 'block'
+        }, timeIniciar)
+    }
+}
