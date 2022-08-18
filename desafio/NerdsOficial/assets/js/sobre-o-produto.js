02950b7d004e999a6ad60ddc8423a9f7f273137a
Wender Natanael <93059850+Wender101@users.noreply.github.com>
2022-08-18 18:56:15 -0300

    a
    w

--- desafio/NerdsOficial/assets/js/sobre-o-produto.js
+++ desafio/NerdsOficial/assets/js/sobre-o-produto.js
@@ -1,5 +1,20 @@
+//! Vai pegar do browser o produto que foi salvo
 const sobreProduto1 = localStorage.getItem('sobreProduto')
 const sobreProduto2 = JSON.parse(sobreProduto1)
+
+//! Vai pegar do browser o carrinho
+const carrinho1 = localStorage.getItem('carrinho')
+const carrinho2 = JSON.parse(carrinho1)
+let carrinho = []
+
+try {
+    for (let c = 0; c < carrinho2.length; c++) {
+        carrinho.push(carrinho2[c])
+    }
+
+} catch {
+    carrinho = []
+}
 
 document.querySelector('title').innerText = sobreProduto2.titulo
 
@@ -33,7 +48,7 @@ function img() {
     } else {
         imgProduto.src = `${novoLink4}.png`
     }
-
+    
 } img()
 
 //! img1
@@ -41,20 +56,36 @@ let img1 = document.getElementById('img1')
 img1.src = sobreProduto2.imgProduto
 
 // * Vai alterar a img principal para a img1
-img1.addEventListener('click', () => {
-    imgProduto.src = img1.src
-})
+img1.addEventListener('click', () => imgProduto.src = img1.src)
 
 //! img2
 let img2 = document.getElementById('img2')
 img2.src = imgProduto.src
 
 // * Vai alterar a img principal para a img2
-img2.addEventListener('click', () => {
-    imgProduto.src = img2.src
-})
+img2.addEventListener('click', () => imgProduto.src = img2.src)
 
 document.getElementById('titulo').innerText = sobreProduto2.titulo
 document.getElementById('desc').innerText = sobreProduto2.desc
 
-document.getElementById('btnContato').href = `https://api.whatsapp.com/send?phone=+55%2061%2099935-2015&text=Estou interessado nesse produto: ${sobreProduto2.titulo}, link: ${img2.src}`
+//! Vai inviar uma msg ao vendedor informando qual é o produto
+document.getElementById('btnContato').href = `https://api.whatsapp.com/send?phone=+55%2061%2099935-2015&text=Estou interessado nesse produto: ${sobreProduto2.titulo}, link: ${imgProduto.src}`
+
+//! Vai add ao carrinho
+function addCarrinho() {
+    let produto = {
+        titulo: sobreProduto2.titulo,
+        desc: sobreProduto2.desc,
+        imgProduto: sobreProduto2.imgProduto
+    }
+
+    carrinho.push(produto)
+    let salvarCarrinho = JSON.stringify(carrinho)
+    localStorage.setItem('carrinho', salvarCarrinho)
+
+    document.getElementById('carrinho').className = 'animation'
+
+    setTimeout(() => {
+        document.getElementById('carrinho').className = ''
+    }, 700)
+}
