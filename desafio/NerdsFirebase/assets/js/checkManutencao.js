a8bb8a09b8a32a2cc895f70e9a787f432732b3f7
Wender Natanael <93059850+Wender101@users.noreply.github.com>
2022-11-11 14:45:04 -0300

    Update checkManutencao.js

--- desafio/NerdsFirebase/assets/js/checkManutencao.js
+++ desafio/NerdsFirebase/assets/js/checkManutencao.js
@@ -4,13 +4,13 @@ db.collection('Manutenção').onSnapshot((data) => {
         let checkManutencao = manutencao.data().EmManutencao
         
         if(checkManutencao == true && window.location.pathname != '/Manutencao.html') {
-            window.location.pathname = '/Manutencao.html'
+            window.location.pathname = 'https://wender101.github.io/html-css/desafio/NerdsFirebase/Manutencao.html'
             setTimeout(() => {
                 location.reload()
             }, 300)
 
         } else if(checkManutencao == false && window.location.pathname == '/Manutencao.html') {
-            window.location.pathname = '/home.html'
+            window.location.pathname = 'https://wender101.github.io/html-css/desafio/NerdsFirebase/home'
             setTimeout(() => {
                 window.history.back()
             }, 300)
