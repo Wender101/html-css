33fa9e1bcdeb6b72e39f82d5ed8a0dd571889920
Wender Natanael <93059850+Wender101@users.noreply.github.com>
2022-12-23 14:10:19 -0300

    a

--- desafio/SocialMedia/assets/js/index.js
+++ desafio/SocialMedia/assets/js/index.js
@@ -308,6 +308,4 @@ window.addEventListener("scroll", (event) => {
         document.getElementById('sobreContatoScroll').id = 'sobreContato'
         document.getElementById('recadoScroll0').innerText = 'Esse é o inicio de uma boa conversa'
     }
-
-    console.log(scroll);
 })
