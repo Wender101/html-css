d4f813a2fa5523a4a65b8af561599d2b285a1713
Wender Natanael <93059850+Wender101@users.noreply.github.com>
2022-12-23 00:33:04 -0300

    Update personalizar.js

--- desafio/SocialMedia/assets/js/personalizar.js
+++ desafio/SocialMedia/assets/js/personalizar.js
@@ -1,252 +1,89 @@
-let emailEncontrado = false
-let cloneContatos = ''
-let codigoLocalUser
-let codigoPessoal
-db.collection('SobreUser').onSnapshot((data) => {
-    data.docs.map(function(val) {
-        let valorSobreUser = val.data()
+//? Vai gerar o codigo para o user
+var respostaFinal = ''
+function gerarCodigo(max = 0) {
+    const letras = 'aabcdefghijklmnopqrstuvwxyz' 
+    const letrasMaiuculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const simbolos = '!@#$%¨&*()_+=-'
+    const numeros = '1234567890'
 
-        try {
-            if(valorSobreUser.Sobre.Email == email && emailEncontrado == false) {
-                emailEncontrado = true
-                codigoLocalUser = val.id
-                cloneContatos = valorSobreUser
-                codigoPessoal = valorSobreUser.Sobre.Codigo
-                var carregando = document.getElementById('carregando')
-                carregando.style.display = 'none'
-    
-                for(let c = 0; c <= valorSobreUser.Contatos.length; c++) {
-                    let ultimaMsgEnviada = ''
+    for(let c = 0; c < max; c++) {
+        if(c < max) {
+            let num1 = Math.floor(Math.random() * 14) 
+            let resSimbolo = simbolos.charAt(num1) 
+            respostaFinal += resSimbolo
+        } 
 
-                    try {
-                        if(valorSobreUser.Contatos[c].Msgs[valorSobreUser.Contatos[c].Msgs.length - 1].MsgContato != undefined) {
-                            ultimaMsgEnviada = valorSobreUser.Contatos[c].Msgs[valorSobreUser.Contatos[c].Msgs.length - 1].MsgContato
-                        } else if(valorSobreUser.Contatos[c].Msgs[valorSobreUser.Contatos[c].Msgs.length - 1].TuaMsg != undefined) {
-                            ultimaMsgEnviada = valorSobreUser.Contatos[c].Msgs[valorSobreUser.Contatos[c].Msgs.length - 1].TuaMsg
-                        }
-                    } catch {}
+        if(c < max) {
+            let num1 = Math.floor(Math.random() * 26) 
+            let resLetrasMinusculas = letras.charAt(num1) 
+            respostaFinal += resLetrasMinusculas
+        } 
 
-                    criaContatos(valorSobreUser.Contatos[c].SobreContato.FotoPerfil, valorSobreUser.Contatos[c].SobreContato.Nome, ultimaMsgEnviada, valorSobreUser.Contatos[c].SobreContato.CodigoContato)
-                }
-            }
-        } catch {}
+        if(c < max) {
+            let num3 = Math.floor(Math.random() * 10) 
+            let resNumeros = numeros.charAt(num3)
+            respostaFinal += resNumeros
 
-        setInterval(() => {
-            if(emailEncontrado == false) {
-                //? Vai mandar o user para a pagina de login
-                const link = location.origin + location.pathname
-                window.location.href = link.replace('home.html', '') + 'login.html'
-            }
-        }, 4000)
-        
-    })
-})
+        }
 
-let codigoContatoSelecionado
-function criaContatos(imagem, nome, ultMsg, codigoContato) {
-    let localContatos = document.getElementById('localContatos')
-    let contatos = document.createElement('div')
-    let lineOnline = document.createElement('div')
-    let img = document.createElement('img')
-    let localNomeEmsgContato = document.createElement('div')
-    let nameContato = document.createElement('strong')
-    let ultimaMsg = document.createElement('p')
+        if(c < max) {
+            let num2 = Math.floor(Math.random() * 26) 
+            let resletrasMaiuculas = letrasMaiuculas.charAt(num2)
+            respostaFinal += resletrasMaiuculas
+        }
+    }
 
-    //? --
-    img.src = imagem
-    nameContato.innerText = nome
-    ultimaMsg.innerText = ultMsg
+    document.getElementById('codigo').innerText = respostaFinal
+} gerarCodigo(5)
 
-    
-    //? Classes
-    contatos.className = 'contatos'
-    lineOnline.className = 'lineOnline'
-    localNomeEmsgContato.className = 'localNomeEmsgContato'
-    nameContato.className = 'nameContato'
-    
-    //? AppendChilds
-    lineOnline.appendChild(img)
-    localNomeEmsgContato.appendChild(nameContato)
-    localNomeEmsgContato.appendChild(ultimaMsg)
-    contatos.appendChild(lineOnline)
-    contatos.appendChild(localNomeEmsgContato)
-    localContatos.appendChild(contatos)
-
-
-    //? fuunções de clique
-    contatos.addEventListener('click', () => {
-        document.getElementById('localMsg').innerHTML = ''
-        document.getElementById('recadoInicial').style.display = 'none'
-        document.getElementById('localConversa').style.display = 'block'
-        document.getElementById('msg').style.display = 'block'
-
-        document.getElementById('imgContato').src = imagem
-        document.getElementById('nomeContato').innerText = nome
-
-        criaMsg(codigoContato)
-        codigoContatoSelecionado = codigoContato
-    })
-}
-
-//? Função que vai criar as msgs
-let arrayCloneChat
-function criaMsg(codigo) {
-    db.collection('SobreUser').onSnapshot((data) => {
-        data.docs.map(function(val) {
-            let valorSobreUser = val.data()
-    
-            try {
-                if(valorSobreUser.Sobre.Email == email) {
-                    for(let c = 0; c <= valorSobreUser.Contatos.length; c++) {
-                        for(let b = 0; b <= valorSobreUser.Contatos[c].Msgs.length; b++) {
-
-                            //? Vai mostrar as msg apenas do contato selecionado
-                            if(valorSobreUser.Contatos[c].SobreContato.CodigoContato == codigo) {
-                                let p = document.createElement('p')
-                                arrayCloneChat = valorSobreUser.Contatos
-
-                                if(valorSobreUser.Contatos[c].Msgs[b].MsgContato != undefined) {
-                                    p.className = 'msgContato'
-                                    p.innerText = valorSobreUser.Contatos[c].Msgs[b].MsgContato
-
-                                } else if(valorSobreUser.Contatos[c].Msgs[b].TuaMsg != undefined) {
-                                    p.className = 'tuaMsg'
-                                    p.innerText = valorSobreUser.Contatos[c].Msgs[b].TuaMsg
-                                }
-
-                                document.getElementById('localMsg').appendChild(p)
-                            }
-                        }
-                    }
-                }
-            } catch {}
-        })
-    })
-}
-
-//? Vai checar se algo foi escrito no input de msg e liberar o btn enviar
+//? Vai checar se os inputs foram respondidos e destravar o btn
+let prontoPress = false
 setInterval(() => {
-    let inputMsg = document.getElementById('inputResponder').value
-    if(inputMsg.length > 0) {
-        document.getElementById('imgEnviar').style.display = 'block'
-        document.getElementById('pet').style.display = 'none'
-    } else {
-        document.getElementById('imgEnviar').style.display = 'none'
-        document.getElementById('pet').style.display = 'block'
+    const inputNome = document.getElementById('inputNome').value
+    const inputRecado = document.getElementById('inputRecado').value
+    const imgUser = document.getElementById('imgUser').src
+
+    if(inputNome.length > 0 && inputRecado.length > 0) {
+        const btnPronto = document.getElementById('btnPronto')
+        btnPronto.innerText = 'Pronto'
+        btnPronto.style.background = '#8ddce2'
+        btnPronto.style.color = '#fff'
+
+        btnPronto.addEventListener('click', () => {
+            if(prontoPress == false) {
+                prontoPress = true
+                cadastrarUSer(imgUser, document.getElementById('inputNome').value, document.getElementById('inputRecado').value, email, respostaFinal)
+            }
+        })
     }
 }, 100)
 
-//? Vai enviar uma msg
-document.addEventListener('keypress', (e) => {
-    if(e.keyCode == 13) {
-        enviarMsg()
+function cadastrarUSer(img, nome, recado, emailUser, codigo) {
+
+    const objFinal = {
+        Sobre: {
+            FotoPerfil: img,
+            Nome: nome,
+            Recado: recado,
+            Email: emailUser,
+            Codigo: codigo,
+        },
+
+        Contatos: []
     }
-})
 
-function enviarMsg() {
-    let msgEnviada = false
-    let inputMsg = document.getElementById('inputResponder').value
-    for(let c = 0; c < arrayCloneChat.length; c++) {
-        if(inputMsg.length > 0 && codigoContatoSelecionado == arrayCloneChat[c].SobreContato.CodigoContato) {
-            let obj = {
-                TuaMsg: inputMsg
-            }
-            arrayCloneChat[c].Msgs.push(obj)
-            db.collection('SobreUser').doc(codigoLocalUser).update({Contatos: arrayCloneChat})
+    
+    db.collection('SobreUser').add(objFinal)
+}
 
-            document.getElementById('localMsg').innerHTML = ''
-            document.getElementById('localContatos').innerHTML = ''
-            document.getElementById('inputResponder').value =  ''
-
-            db.collection('SobreUser').onSnapshot((data) => {
-                data.docs.map(function(val) {
-                    let valorSobreUser = val.data()
-                    try {
-                        if(valorSobreUser.Sobre.Codigo == codigoContatoSelecionado) {
-                            for(let c = 0; c < valorSobreUser.Contatos.length; c++) {
-                                if(valorSobreUser.Contatos[c].SobreContato.CodigoContato == codigoPessoal &&msgEnviada == false) {
-                                    msgEnviada = true
-                                    let arrayCloneChat2 = valorSobreUser.Contatos
-                                    let obj2 = {
-                                        MsgContato: inputMsg
-                                    }
-                                    arrayCloneChat2[c].Msgs.push(obj2)
-                                    db.collection('SobreUser').doc(val.id).update({Contatos: arrayCloneChat2})
-                                }
-                            }
-                        }
-                    } catch {}
-                })
-            })
+db.collection('SobreUser').onSnapshot((data) => {
+    data.docs.map(function(val) {
+    if(prontoPress == true) {
+        setTimeout(() => {
+            //? Vai mandar o user para o home
+            const link = location.origin + location.pathname
+            window.location.href = link.replace('Personalizar.html', '') + 'home.html'
+        }, 2000)
         }
-    }
-}
-
-//? Função add Contato
-let localAdd = document.getElementById('localAdd')
-function abrirAddContato() {
-    localAdd.style.display = 'flex'
-}
-
-localAdd.addEventListener('click', (e) => {
-    let el = e.target.id
-    if(el == 'localAdd' || el == 'btnAddContato') {
-        localAdd.style.display = 'none'
-    }
-}) 
-
-//? função de adicionar contato
-function addContato() {
-    let adicionado = false
-    let cogidoDoContatoAdicionado = ''
-    let inputAddContato = document.getElementById('inputAddContato').value
-
-    db.collection('SobreUser').onSnapshot((data) => {
-        data.docs.map(function(val) {
-            let valorSobreUser = val.data()
-
-            
-            try {
-                //? Vai adicionar ao seus conatatos uma nova pessoa
-                if(inputAddContato.length > 0 && valorSobreUser.Sobre.Codigo == inputAddContato && adicionado == false) {
-                    adicionado = true
-                    cogidoDoContatoAdicionado = val.id
-
-                    let objContatos = {
-                        Msgs: [],
-                
-                        SobreContato: {
-                            FotoPerfil: valorSobreUser.Sobre.FotoPerfil,
-                            Nome: valorSobreUser.Sobre.Nome,
-                            Recado: valorSobreUser.Sobre.Recado,
-                            CodigoContato: valorSobreUser.Sobre.Codigo,
-                        }
-                    }
-
-                    cloneContatos.Contatos.push(objContatos)
-                    db.collection('SobreUser').doc(codigoLocalUser).update({Contatos: cloneContatos.Contatos})
-                    criaContatos(valorSobreUser.Sobre.FotoPerfil, valorSobreUser.Sobre.Nome, '...', valorSobreUser.Sobre.Codigo)
-                }
-
-                //? vai salvar seu contato na parte contatos do individuo q vc adiconou
-                if(valorSobreUser.Sobre.Email == email) {
-                    let cloneContatos2 = valorSobreUser
-                    let objContatos2 = {
-                        Msgs: [],
-                
-                        SobreContato: {
-                            FotoPerfil: valorSobreUser.Sobre.FotoPerfil,
-                            Nome: valorSobreUser.Sobre.Nome,
-                            Recado: valorSobreUser.Sobre.Recado,
-                            CodigoContato: valorSobreUser.Sobre.Codigo,
-                        }
-                    }
-
-                    cloneContatos2.Contatos.push(objContatos2)
-
-                    db.collection('SobreUser').doc(cogidoDoContatoAdicionado).update({Contatos: cloneContatos2.Contatos})
-                }
-            } catch {}
-        })
     })
-}
+})
