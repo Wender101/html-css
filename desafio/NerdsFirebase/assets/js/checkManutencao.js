11c4e7cea3e40ff190685eee5ac6f3b95024c044
Wender Natanael <93059850+Wender101@users.noreply.github.com>
2023-01-04 15:08:44 -0300

    a

--- desafio/NerdsFirebase/assets/js/checkManutencao.js
+++ desafio/NerdsFirebase/assets/js/checkManutencao.js
@@ -4,15 +4,14 @@ db.collection('Manutenção').onSnapshot((data) => {
 
         const checkManutencao = manutencao.data().EmManutencao
         
-        if(checkManutencao == true && window.location.href != 'https://wender101.github.io/html-css/desafio/NerdsFirebase/Manutencao.html') {
-        // if(checkManutencao == true && window.location.href != 'http://127.0.0.1:5501/Manutencao.html') {
-            // window.location.href = 'http://127.0.0.1:5501/Manutencao.html'
-            window.location.href = 'https://wender101.github.io/html-css/desafio/NerdsFirebase/Manutencao.html'
+        if(checkManutencao == true && window.location.href != 'http://127.0.0.1:5501/Manutencao.html') {
+            window.location.href = 'http://127.0.0.1:5501/Manutencao.html'
+            // window.location.href = 'https://wender101.github.io/html-css/desafio/NerdsFirebase/Manutencao.html'
             setTimeout(() => {
                 location.reload()
             }, 100)
 
-        } else if(checkManutencao == false && window.location.href == 'https://wender101.github.io/html-css/desafio/NerdsFirebase/Manutencao.html') {
+        } else if(checkManutencao == false && window.location.href == 'http://127.0.0.1:5501/Manutencao.html') {
             window.history.back()
         }
     })
