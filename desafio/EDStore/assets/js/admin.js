8dcc651340aca5af2f6dce76ee7e60cee0ac8688
Wender Natanael <93059850+Wender101@users.noreply.github.com>
2023-01-05 20:07:36 -0300

    Add files via upload

---
+++ desafio/EDStore/assets/js/admin.js
@@ -0,0 +1,234 @@
+let main  = document.querySelector('main')
+//? Vai abrir o pop-up de adicioanar um produto
+function abrirAddProduto() {
+    document.getElementById('popUpAddProd').style.display = 'block'
+}
+
+//? Vai fechar o pop-up de adicioanar um produto
+function fecharPopUp() {
+    document.getElementById('popUpAddProd').style.display = 'none'
+    limpar()
+}
+
+//? Vai limpar os inputs de add
+function limpar() {
+    let imagem1 = document.getElementsByClassName('imgProd')[0].value = ''
+    let imagem2 = document.getElementsByClassName('imgProd')[1].value = ''
+    let imagem3 = document.getElementsByClassName('imgProd')[2].value = ''
+    let imagem4 = document.getElementsByClassName('imgProd')[3].value = ''
+    let name = document.getElementById('nomeProdutoAdd').value = ''
+    let desccription = document.getElementById('descProd').value = ''
+    let val = document.getElementById('valorProd').value = ''
+    let descontoProd = document.getElementById('descontoInput').value = ''
+
+    let imgEX1 = document.getElementsByClassName('imgEX')[0].src = ''
+    let imgEX2 = document.getElementsByClassName('imgEX')[1].src = ''
+    let imgEX3 = document.getElementsByClassName('imgEX')[2].src = ''
+    let imgEX4 = document.getElementsByClassName('imgEX')[3].src = ''
+    let imgPrincipal = document.getElementsByClassName('imgPrincipal')[0].src = ''
+    let imgPrincipal2 = document.getElementsByClassName('imgPrincipal')[1].src = ''
+    let idDoProduto = document.getElementById('idDoProduto').value = ''
+    let tags = document.getElementById('tags').value = ''
+    let descDetalhada = document.getElementById('descDetalhada').value = ''
+}
+
+//? Vai adicionar as informações do produto
+let valInput = 0
+let valInput2 = 0
+let valInput3 = 0
+let valInput4 = 0
+let valInput5 = 0
+let prontoPraAdd = false
+setInterval(() => {
+    for (let c = 0; c < 4; c++) {
+        try {
+            let inputImg = document.getElementsByClassName('imgProd')[c].value
+            let nameProd = document.getElementById('nomeProdutoAdd').value
+            let desc = document.getElementById('descProd').value
+            let valorProd = document.getElementById('valorProd').value
+            let descontoInput = document.getElementById('descontoInput').value
+            let idDoProduto = document.getElementById('idDoProduto').value
+            let descDetalhada = document.getElementById('descDetalhada').value
+    
+            if(inputImg.length != valInput) {
+                valInput = inputImg.length
+                let imgEX = document.getElementsByClassName('imgEX')[c]
+                document.getElementsByClassName('imgPrincipal')[0].src = document.getElementsByClassName('imgProd')[0].value
+                document.getElementsByClassName('imgPrincipal')[1].src = document.getElementsByClassName('imgProd')[0].value
+                try {
+                    imgEX.src = inputImg
+                    
+                } catch (error) {
+                    
+                }
+            }
+
+            if(nameProd.length != valInput2) {
+                valInput2 = nameProd.length
+                document.getElementsByClassName('nameProd')[0].innerHTML = nameProd
+
+                document.getElementsByClassName('nameProd')[1].innerHTML = nameProd
+            }
+
+            if(desc.length != valInput3) {
+                if(desc.length == 236 && valInput3 != desc.length) {
+                    alert('Você excedeu o máximo de caracteres na descrição resumida')
+                } else {
+                    document.getElementById('desc').innerText = desc
+                }
+                valInput3 = desc.length
+            }
+
+            if(descDetalhada.length != valInput4) {
+                valInput4 = descDetalhada.length
+                document.getElementById('descricaoP').innerHTML = descDetalhada
+            }
+
+            if(descontoInput.length != valInput5) {
+                valInput5 = descontoInput.length
+                if(descontoInput <= 0) {
+                    document.getElementById('qDesconto').innerText = '0% OFF'
+                } else {
+                    document.getElementById('qDesconto').innerText = descontoInput + '%'
+                }
+            }
+
+            // //? Vai calcular o desconto
+            if(valorProd.length >= 0 || descontoInput.length != valInput5) {
+                if(descontoInput <= 0) {
+                    descontoInput = 0
+                }
+                let valorComDesconto = (((descontoInput * valorProd) / 100) - valorProd) * -1
+                var res = valorComDesconto
+                if(descontoInput != 0) {
+                    res = (valorProd - valorComDesconto) + valorComDesconto
+                }
+
+                document.getElementById('valorNormal').innerText = 'R$' + res
+                document.getElementsByClassName('valorProdAdd')[0].innerText = 'R$' + valorComDesconto.toFixed(2)
+                document.getElementsByClassName('valorProdAdd')[1].innerText = 'R$' + valorComDesconto.toFixed(2)
+                document.getElementById('valorSemDc').innerText = 'R$' + res.toFixed(2)
+                document.getElementById('salvo').innerText = 'Salvo - R$' + (res - valorComDesconto).toFixed(2)
+            }
+
+            //? Vai chevcar se todos os inputs importantes foram respondidos
+            if(document.getElementsByClassName('imgProd')[0].value.length > 0 && nameProd.length > 0 && desc.length > 0 && desc.length <= 236 && valorProd.length > 0 && idDoProduto.length > 0) {
+                prontoPraAdd = true
+                document.getElementById('AddBtn').style.background = '#008ecc'
+                document.getElementById('AddBtn').style.cursor = 'pointer'
+            } else {
+                prontoPraAdd = false
+                document.getElementById('AddBtn').style.background = '#666'
+                document.getElementById('AddBtn').style.cursor = 'auto'
+            }
+        } catch (error){console.warn(error);}
+    }
+}, 100)
+
+//? Vai adicionar o produto
+function addProduto() {
+    if(prontoPraAdd == true) {
+        let imagem1 = document.getElementsByClassName('imgProd')[0].value
+        let imagem2 = document.getElementsByClassName('imgProd')[1].value
+        let imagem3 = document.getElementsByClassName('imgProd')[2].value
+        let imagem4 = document.getElementsByClassName('imgProd')[3].value
+        let name = document.getElementById('nomeProdutoAdd').value
+        let desccription = document.getElementById('descProd').value
+        let val = document.getElementById('valorProd').value
+        let descontoProd = document.getElementById('descontoInput').value
+        let idDoProduto = document.getElementById('idDoProduto').value
+        let descDetalhada = document.getElementById('descDetalhada').value
+        let tags = document.getElementById('tags').value
+        
+        let obj = {
+            Img1: imagem1,
+            Img2: imagem2,
+            Img3: imagem3,
+            Img4: imagem4,
+            Nome: name,
+            Desc: desccription,
+            DescDetalhada: descDetalhada,
+            Valor: val,
+            Desconto: descontoProd,
+            Id: idDoProduto,
+            Tags: tags
+        }
+
+        db.collection('Produtos').add(obj)
+        fecharPopUp()
+        main.innerHTML = ''
+    }
+}
+
+//? Vai carregar o produto na tela
+function carregarProduto() {
+    main.innerHTML = ''
+    db.collection('Produtos').onSnapshot((data) => {
+        data.docs.map(function(val) {
+            let Produtos = val.data()
+
+            criaProduto(Produtos.Img1, Produtos.Img2, Produtos.Img3, Produtos.Img4, Produtos.Nome, Produtos.Desc , Produtos.Valor, Produtos.Desconto, Produtos.Id)
+        })
+    })
+} carregarProduto()
+
+function criaProduto(Img1 ,Img2, Img3, Img4, Nome, Desc, Valor, Desconto, Id) {
+    let prod = document.createElement('div')
+    let localImg = document.createElement('div')
+    let imgProduto = document.createElement('img')
+    let sobreProd = document.createElement('div')
+    let nameProd = document.createElement('p')
+    let p = document.createElement('p')
+    let valorStrong = document.createElement('strong')
+    let valorSemDescontoT = document.createElement('span')
+    let valorSalvo = document.createElement('span')
+
+    //? Class
+    prod.className = 'prod'
+    localImg.className = 'localImg'
+    imgProduto.className = 'imgProduto'
+    sobreProd.className = 'sobreProd'
+    nameProd.className = 'nameProd'
+    valorStrong.className = 'valor'
+    valorSemDescontoT.className = 'valorSemDescontoT'
+    valorSalvo.className = 'valorSalvo'
+
+    //?---
+    imgProduto.src = Img1
+    nameProd.innerText = Nome
+
+    if(Desconto <= 0) {
+        Desconto = 0
+    }
+    let valorComDesconto = (((Desconto * Valor) / 100) - Valor) * -1
+    var res = valorComDesconto
+    if(Desconto != 0) {
+        res = (Valor - valorComDesconto) + valorComDesconto
+    }
+
+    valorStrong.innerText = 'R$' + valorComDesconto.toFixed(2)
+    valorSemDescontoT.innerText = 'R$' + parseInt(Valor).toFixed(2)
+    valorSalvo.innerText = 'Salvo - R$' + (res - valorComDesconto).toFixed(2)
+    
+    //? AppendChild
+    p.appendChild(valorStrong)
+    p.appendChild(valorSemDescontoT)
+    localImg.appendChild(imgProduto)
+    sobreProd.appendChild(nameProd)
+    sobreProd.appendChild(p)
+    sobreProd.appendChild(valorSalvo)
+    prod.appendChild(localImg)
+    prod.appendChild(sobreProd)
+    main.appendChild(prod)
+
+    //? Funções de click
+    prod.addEventListener('click', () => {
+        localStorage.setItem('sobreProduto', Id)
+        if(location.host == '127.0.0.1:5500') {
+            location.pathname = '/Sobre-Produto.html'
+            
+        } else if(location.host == 'wender101.github.io') {
+            location.href = 'https://wender101.github.io/html-css/desafio/EDStore/Sobre-Produto.html'
+        }
+    })
+}
