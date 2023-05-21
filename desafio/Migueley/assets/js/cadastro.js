67679bbf40d286f6fd8aec38b8e367928411d200
Wender Natanael <93059850+Wender101@users.noreply.github.com>
2023-05-21 10:45:59 -0300

    Delete desafio/Migueley/assets/js directory

--- desafio/Migueley/assets/js/cadastro.js
+++
@@ -1,9 +0,0 @@
-function cadastrar() {
-    let input = document.querySelector('input')
-    if(input.value.length <= 0) {
-        alert('Você não pode fazer um cadastro ou logar em sua conta sem informar um nome antes.')
-
-    } else {
-        login()
-    }
-}
